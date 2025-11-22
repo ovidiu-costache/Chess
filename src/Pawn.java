8ee80b888635a9a4d7075ffcee9bc3dcab875229
ovidiu-costache <ovidiu.costache2110@stud.acs.upb.ro>
2025-11-22 19:33:35 +0200

    Pawn can now move

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.awt.*;
+import java.rmi.UnexpectedException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,8 +12,61 @@ public class Pawn extends Piece {
 
     @Override
     public List<Position> getPossibleMoves(Board board) {
-        // WILL BE IMPLEMENTED LATER
-        return new ArrayList<>();
+        List<Position> moves = new ArrayList<>();
+        Position current = getPosition();
+        int currentRow = current.getRow();
+        char currentCol = current.getCol();
+
+        // WHITE + 1, BLACK - 1
+        int direction;
+        if (this.getColor() == Colors.WHITE)
+            direction = 1;
+        else
+            direction = -1;
+
+        // A step forward
+        int nextRow = currentRow + direction;
+        if (nextRow >= 1 && nextRow <= 8) {
+            Position forward1 = new Position(currentCol, nextRow);
+
+            if (board.getPieceAt(forward1) == null) {
+                moves.add(forward1);
+
+                // 2 steps forward if it's first move
+                boolean isFirst = false;
+                if (this.getColor() == Colors.WHITE && currentRow == 2)
+                    isFirst = true;
+                if (this.getColor() == Colors.BLACK && currentRow == 7)
+                    isFirst = true;
+
+                if (isFirst) {
+                    int nextRow2 = currentRow + (2 * direction);
+                    Position forward2 = new Position(currentCol, nextRow2);
+
+                    if (board.getPieceAt(forward2) == null)
+                        moves.add(forward2);
+                }
+            }
+        }
+
+        // Diagonal movement
+        int[] cols = {-1, 1};
+
+        for (int offset : cols) {
+            char attackCol = (char) (currentCol + offset);
+            int attackRow = currentRow + direction;
+
+            if (attackCol >= 'A' && attackCol <= 'H' && attackRow >= 1 && attackRow <= 8) {
+                Position attackPos = new Position(attackCol, attackRow);
+                Piece target = board.getPieceAt(attackPos);
+
+                if (target != null)
+                    if (target.getColor() != this.getColor())
+                        moves.add(attackPos);
+            }
+        }
+
+        return moves;
     }
 
     @Override
