b24dae83f186701f75447d81ce4577e5c7378761
ovidiu-costache <ovidiu.costache2110@stud.acs.upb.ro>
2025-11-22 19:33:49 +0200

    Rook can now move

--- src/Rook.java
+++ src/Rook.java
@@ -10,8 +10,66 @@ public class Rook extends Piece {
 
     @Override
     public List<Position> getPossibleMoves(Board board) {
-        // WILL BE IMPLEMENTED LATER
-        return new ArrayList<>();
+        List<Position> move = new ArrayList<>();
+        Position current = getPosition();
+        int currentRow = current.getRow();
+        char currentCol = current.getCol();
+
+        int row;
+        for (row = currentRow + 1; row <= 8; row++) {
+            Position pos = new Position(currentCol, row);
+            Piece target = board.getPieceAt(pos);
+
+            if (target == null)
+                move.add(pos);
+            else {
+                if (target.getColor() != this.getColor())
+                    move.add(pos);
+                break;
+            }
+        }
+
+        for (row = currentRow - 1; row >= 1; row--) {
+            Position pos = new Position(currentCol, row);
+            Piece target = board.getPieceAt(pos);
+
+            if (target == null)
+                move.add(pos);
+            else {
+                if (target.getColor() != this.getColor())
+                    move.add(pos);
+                break;
+            }
+        }
+
+        char col;
+        for (col = (char) (currentCol + 1); col <= 'H'; col++) {
+            Position pos = new Position(col, currentRow);
+            Piece target = board.getPieceAt(pos);
+
+            if (target == null)
+                move.add(pos);
+            else {
+                if (target.getColor() != this.getColor())
+                    move.add(pos);
+                break;
+            }
+        }
+
+        for (col = (char) (currentCol - 1); col >= 'A'; col--) {
+            Position pos = new Position(col, currentRow);
+            Piece target = board.getPieceAt(pos);
+
+            if (target == null)
+                move.add(pos);
+            else {
+                if (target.getColor() != this.getColor())
+                    move.add(pos);
+                break;
+            }
+        }
+
+        return move;
     }
 
     @Override
